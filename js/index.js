95117efd28eadd20e9bf699208b634f714934dfa
Shekhar Shrestha <shekharr.shresthaa@gmail.com>
2023-10-11 15:01:33 +0545

    feat: inner pages completed

--- js/index.js
+++ js/index.js
@@ -10,25 +10,7 @@ $('.commercial-residential-row').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   prevArrow: '<i class="fa-solid fa-chevron-left left-arrow"></i>',
-  nextArrow: '<i class="fa-solid fa-chevron-right right-arrow"></i>',
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+  nextArrow: '<i class="fa-solid fa-chevron-right right-arrow"></i>'
 });
 
 
